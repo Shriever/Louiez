817e03ce2a3efd4fa5a43502f77c798dac46c64b
Levi Lieberman <ShrieverDev@gmail.com>
2021-11-11 16:08:49 -0800

    initial commit

---
+++ transfer.js
@@ -0,0 +1,10 @@
+const LOCATION = "1446 ocean drive"
+// parking is $2 per hour, no reservation, off site, public
+const languages = ['english', 'spanish']
+const data = {
+    airbnbLink: "https://www.airbnb.com/rooms/32474818?source_impression_id=p3_1636671547_Ta7OAbmm6TflKjzk",
+    apartmentNumber: 5,
+    price: 110,
+    businessTaxReceiptNumber: "123abc",
+    resortTaxRegistrationCertificateNumber: "123abc"
+},
