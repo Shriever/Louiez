003469ab64d8d771cd4e9a858a099c24ec68d216
Levi Lieberman <ShrieverDev@gmail.com>
2021-11-18 17:35:21 -0800

    bugs. bugs everywhere.

--- src/const.ts
+++ src/const.ts
@@ -2,3 +2,7 @@ export const LOCATION = '1446 ocean drive';
 
 export const BOOKING_URL =
   'https://account.booking.com/sign-in?op_token=EgVvYXV0aCJ6ChQ2Wjcyb0hPZDM2Tm43emszcGlyaBIJYXV0aG9yaXplGiVodHRwczovL2pvaW4uYm9va2luZy5jb20vYWNjb3VudC5odG1sKip7InBhZ2UiOiIvcmVkaXJlY3QtdG8tam9pbmFwcC1scD9sYW5nPXh1In1CBGNvZGUqFBACMJLNhe_CmyU6AEIAWJX6towG';
+
+
+  export const CREATE_LISTING_URL =
+    'https://join.booking.com/category/index.html?token=cb8cac3fdaf2871121e4d62c2ba51c0e809ec5f6&waypoint_id=176406366&aid=304142&label=gen173nr-1DCAEoggI46AdIM1gEaIkCiAEBmAExuAEYyAEM2AED6AEB-AECiAIBqAIEuALX69uMBsACAdICJGFiYmUwNThkLTYzMTQtNDlmOS1hZTY5LTdkYzcyN2VmNGQ0M9gCBOACAQ';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import puppeteer from 'puppeteer';
-import { BOOKING_URL } from './const';
+import { BOOKING_URL, CREATE_LISTING_URL, LOCATION } from './const';
 
 const sleep = (delay: number = 1) => {
   return new Promise(resolve => setTimeout(resolve, delay * 1000));
@@ -20,6 +20,8 @@ const data = {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   const phoneSelectorValue = 'cba84b379c459e868f2b8da5c1f7df8c';
+
+  // LOGIN
   await page.goto(BOOKING_URL);
 
   await page.type('#loginname', process.env.EMAIL as string);
@@ -40,13 +42,17 @@ const data = {
   await page.click('.bui-spacer button._2emQHDWTNbBMVHK80Tj0A2.nv-request-tfa');
 
   await sleep(30);
-  // await page.
 
-  await Promise.all([
-    page.waitForNavigation(),
-    page.click('button.bui-button span.bui-button__text'),
-  ]);
+  // ADD NEW PROPERTY LISTING
 
+  await page.goto(CREATE_LISTING_URL);
+  // await Promise.all([
+  //   page.waitForNavigation(),
+  //   page.click('button.bui-button span.bui-button__text'),
+  // ]);
+  // await page.click('button.bui-button span.bui-button__text');
+  // await page.waitForNavigation();
+  // SELECT APARTMENT TYPE
   await page.click(
     '#automation_id_choose_category_QUICK_START_APARTMENT_SUP > .card'
   );
@@ -57,19 +63,90 @@ const data = {
   await page.click('#automation_id_nav_button_forward');
   await page.waitForNavigation();
 
+  // IMPORT AIRBNB LISTING
   await page.click(
     '._1X4Mma_goXAEy3ePAPS7v_.automation_class_otaq_airbnb_input'
   );
 
+  await sleep(45);
   await page.type('.airbnb-link-input__input input', data.airbnbLink);
   await page.click('.airbnb-link-input__button');
 
-  await page.waitForSelector(".automation_input_airbnb_block_success")
+  await page.waitForSelector('.automation_input_airbnb_block_success');
 
   await page.click('#automation_id_nav_button_forward');
   await page.waitForNavigation();
 
+  // PROPERTY NAME AND LOCATION
   await sleep(15); // Time to verify that the name is valid
 
+  await page.click('.form-buttons-container--primary button');
+  await page.waitForNavigation();
 
+  await page.type('#address-autocomplete', LOCATION);
+  await page.waitForSelector('button.autocomplete-suggestions__btn');
+  await page.click('button.autocomplete-suggestions__btn');
+
+  await sleep(3);
+  console.log('Primary button 1');
+  await Promise.all([
+    await page.click('.form-buttons-container--primary button'),
+    await page.waitForNavigation(),
+  ]);
+  // await page.click('.form-buttons-container--primary button');
+  // await page.waitForNavigation();
+
+  console.log('Primary button 2');
+  await Promise.all([
+    await page.click('.form-buttons-container--primary button'),
+    await page.waitForNavigation(),
+  ]);
+  // await page.click('.form-buttons-container--primary button');
+  // await page.waitForNavigation();
+
+  console.log('Primary button 3');
+  await Promise.all([
+    await page.click('.form-buttons-container--primary button'),
+    await page.waitForNavigation(),
+  ]);
+  // await page.click('.form-buttons-container--primary button');
+  // await page.waitForNavigation();
+
+  // PROPERTY SETUP
+  console.log('automation');
+  await page.click(
+    '#automation_id_CheckboxList-amenity_id-123facility_id-15 label'
+  );
+
+  await page.click('.form-buttons-container--primary button');
+  await page.waitForNavigation();
+
+  await page.click('.form-buttons-container--primary button');
+  await page.waitForNavigation();
+
+  await page.click('#automation_id_parking_paid div');
+
+  await page.type('#automation_id_parking_input_container', '2');
+
+  await page.select('.a417f321ec select', 'hour');
+
+  await page.click('#automation_id_parking_offsite div');
+
+  await page.click('#automation_id_parking_public div');
+
+  await page.click('.form-buttons-container--primary button');
+  await page.waitForNavigation();
+
+  await page.click('#automation_id_recommended_group > div > div:nth-child(1)');
+  await page.click('#automation_id_recommended_group > div > div:nth-child(2)');
+
+  await page.click('.form-buttons-container--primary button');
+  await page.waitForNavigation();
+
+  await page.click('.form-buttons-container--primary button');
+  await page.waitForNavigation();
+
+  await page.click('.b777be90c1 > label._0ae3340ab3');
+
+  // ADD PHOTOS
 })();
